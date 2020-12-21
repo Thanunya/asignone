deaf2ca455476584ce645a59a8f37eb56ea65ceb
Thanunya <76036829+Thanunya@users.noreply.github.com>
2020-12-21 21:08:42 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,42 +1,46 @@
 <!DOCTYPE html>
 <html lang="th">
 <html>
-  <head>
+<head>
     <meta charset="UTF-8">
-    <title>ประวัติส่วนตัว</title>
-  </head>
-<style>
-body {background-image: url(https://u01.appmifile.com/images/2019/03/12/50b44b01-0ebf-4a0b-886c-580c4f5444c6.jpg);
-    background-repeat: no-repeat;
-    background-attachment: fixed; 
-    background-size: cover;
-    background-position: center;
-    text-align: center;}
-    h1{color:rgb(202, 51, 177); text-align: center;}
-    p {font-family: verdana;  font-size: 20px; color:rgb(180, 50, 137);text-align: center}
-
-</style>
-
+<title>Thanunya</title>
+</head>
 <body>
 
-    <h1>ประวัติส่วนตัว</h1>
-    <p>   <img src="https://scontent.fbkk2-8.fna.fbcdn.net/v/t1.15752-9/131989193_3110275115740283_3485451230259606956_n.jpg?_nc_cat=102&ccb=2&_nc_sid=ae9488&_nc_eui2=AeG9lgfxezUzlcXpUtYR4R0QAD3BzcGqaGAAPcHNwapoYBPI4-k_FXf4NusOxBHdnB1Qfey3ie0jX-5OZvfJmKe7&_nc_ohc=AbfWaB075PgAX8n8mFa&_nc_ht=scontent.fbkk2-8.fna&oh=f73aff85cc77f80397b9937439bbe737&oe=600643C0" width="150px" height="200px" >
-    <p>  <u>ชื่อ-นามสกุล </u> :  นางสาว อรปรียา เที่ยงสัตย์ 
-    <p> <br> <u>รหัสนักศึกษา</u> : 620710409</p>
-    <p>    <br> <u>ชื่อเล่น </u> : จีจี้  อายุ : 20 ปี</p>
-    <p>    <br> <u>ที่อยู่ </u> :345/69 หมู่6 หมู๋บ้านเพอร์เฟคซิตี้ ตำบล บางเพรียง อำเภอบางบ่อ จังหวัด สมุทรปราการ  </p>
-    <p>    <br> <u>วันเกิด </u> : 9 พฤศจิกายน 2000</p>
-    <p>    <br> <u>กำลังศึกษาที่</u> : มหาวิทยาลัยศิลปากร  </p>
-    <p>    <br> <u>งานอดิเรก </u> : ดูหนัง ฟังเพลง กินแล้วก็นอน
-    <p>    <br> <a href="https://www.youtube.com/watch?v=E0IfcXBxyic" target="_blank">ช่วงนี้กำลังอิน </a></p>
-    <p>    <br> <u>เบอร์ติดต่อ</u> : 0923615394</p>
+    <style>
+        body {
+          background-image: url('https://image.freepik.com/free-vector/cloud-background_91008-39.jpg');
+          background-repeat: no-repeat;
+          background-attachment: fixed;  
+          background-size: cover;
+          text-align: center;
+
+        }
+        </style>
+        </head>
+        <body>
         
-    </p>
+<h2 style="color: AliceBlue ;">   R.THANUNYA </h2>
+<img src="https://scontent.fbkk13-2.fna.fbcdn.net/v/t1.15752-9/132142953_894684811338262_5762271616412253842_n.jpg?_nc_cat=111&ccb=2&_nc_sid=ae9488&_nc_eui2=AeGDqjPaszEhmgYmgOhbI2KvKOUCtDBzGN4o5QK0MHMY3nuU--TPYU94J-OHPckJVcEHSLNtlCncsBr5rKi6v4kJ&_nc_ohc=8wTP_00PNiYAX-wBdpm&_nc_ht=scontent.fbkk13-2.fna&oh=32209d710bdfa5f0340b9eb1b7482523&oe=60076B77" alt="3" width="290" height="280">
+<p  style="color: LightSteelBlue;"> รหัสนักศึกษา : 620710160    </p>  
+<p  style="color: LightSteelBlue;"> ชื่อ : ธนัญญา  รอดกำพล    </p>  
+<p  style="color: LightSteelBlue;"> ชื่อเล่น  : ปู </p>
+<p  style="color: LightSteelBlue;"> วันเกิด :  17 มีนาคม 2544 </p>
+<p  style="color: LightSteelBlue;"> ศึกษาอยู่ : มหาวิทยาลัยศิลปากร คณะวิทยาศาสตร์ สาขาวิทยาการข้อมูล ปี2 </p>
+<p  style="color: LightSteelBlue;"> งานอดิเรก/ความชอบ : ดูยูทูป/ซีรี่ส์/ฟังเพลง, อ่านwebtoon, หนัง/นิยายแนวระทึกขวัญ sci-fi  </p>
+
+
+<p  style="color: LightSteelBlue;"> ติดต่อ :</p>
+<p  style="color: LightSteelBlue;" > <img src="https://e7.pngegg.com/pngimages/618/453/png-clipart-mobile-phones-telephone-computer-icons-symbol-miscellaneous-telephone-call.png" 
+    alt="phone" width="20" height="20"> :  089-929-8662 </p>
+
+<p  style="color: LightSteelBlue;" > <img src="https://cdn.icon-icons.com/icons2/1826/PNG/512/4202011emailgmaillogomailsocialsocialmedia-115677_115624.png" 
+        alt="mail" width="20" height="20">  : rodkamphon_t2@silpakorn.edu </p>
+
+<p  style="color: LightSteelBlue;" > <a href="https://web.facebook.com/thanunya.rodkamphon/"> <img src="https://cdn.icon-icons.com/icons2/836/PNG/512/Facebook_icon-icons.com_66805.png" 
+            alt="mail" width="20" height="20"></a>  :  Thanunya Rodkamphon </p>    
+
+<p  style="color: LightSteelBlue;" > <a href="https://www.instagram.com/poo_aynunaht/"> <img src="https://www.dailygizmo.tv/wp-content/uploads/2020/05/instagram-png-instagram-png-logo-1455-300x300.png" 
+            alt="mail" width="20" height="20"></a>  : @poo_aynunaht </p>  
 </body>
 </html>
-
-
-
-
-
-
